0b736f60fe691711d13ee03c9e89fcc53294ff2e
unknown <arkadiuszhorwat@gmail.com>
2021-03-04 15:22:36 +0100

    add js efects to projects section

--- js/projects.js
+++ js/projects.js
@@ -1,23 +1,25 @@
 const daily = document.querySelector('#daily');
 const tic = document.querySelector('#tic');
 const mobile = document.querySelector('#mobile');
-const projInfo = document.getElementsByClassName('.p-info');
+const dailyEl = document.querySelector('.d');
+const ticEl = document.querySelector('.t');
+const mobileEl = document.querySelector('.m');
 
-daily.addEventListener('mouseover', () =>{
-    projInfo.classList.remove('hide');
+daily.addEventListener('mouseover', () => {
+    dailyEl.classList.remove('hide');
 });
-daily.addEventListener('mouseout', () =>{
-    projInfo.classList.add('hide');
+daily.addEventListener('mouseout', () => {
+    dailyEl.classList.add('hide');
 });
-tic.addEventListener('mouseover', () =>{
-    projInfo.classList.remove('hide');
+tic.addEventListener('mouseover', () => {
+    ticEl.classList.remove('hide');
 });
-tic.addEventListener('mouseout', () =>{
-    projInfo.classList.add('hide');
+tic.addEventListener('mouseout', () => {
+    ticEl.classList.add('hide');
 });
-mobile.addEventListener('mouseover', () =>{
-    projInfo.classList.remove('hide');
+mobile.addEventListener('mouseover', () => {
+    mobileEl.classList.remove('hide');
 });
-mobile.addEventListener('mouseout', () =>{
-    projInfo.classList.add('hide');
+mobile.addEventListener('mouseout', () => {
+    mobileEl.classList.add('hide');
 });
