7f9d530ef4047f045cb9511b60e6b605931f62fb
unknown <arkadiuszhorwat@gmail.com>
2021-03-06 14:38:42 +0100

    add simple validation

--- js/email.js
+++ js/email.js
@@ -1,13 +1,23 @@
 const inputMail = document.querySelectorAll('.inputs');
+const form = document.querySelector('form');
+const inputTel = document.querySelector('#phone');
+const alertMessage = document.querySelector('.alert');
 
-const sendEmail=(e)=>{
-    e.preventDefault();
+form.addEventListener('submit', (e)=>{
+  e.preventDefault();
 
+  const numbers = /^[0-9]+$/;
+  if(inputTel.value == '' || inputTel.value.match(numbers) ){
     emailjs.sendForm('service_yekkatu', 'template_jqly22s', e.target, 'user_2q1XBdhAcBHUVtGBKWKvi')
-        .then((result) => {
-        alert('Message was sent');
-        inputMail.forEach(input => input.value = '');
-    }, (error) => {
-      alert('Sending the message failed');
-    });
-}
+      .then((result) => {
+      alertMessage.classList.remove('hide');
+      inputMail.forEach(input => input.value = '');
+  }, (error) => {
+    alert('Sending the message failed');
+  });
+  }
+  else{
+    inputTel.value = '';
+    inputTel.placeholder = "Enter numbers only..";
+  }
+}); 
